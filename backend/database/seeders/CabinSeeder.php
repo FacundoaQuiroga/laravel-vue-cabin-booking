c9fa109843d61b4e5e935a3c02b4cc1e61eed97b
Facundo <facundoaquiroga1@gmail.com>
2026-06-11 12:40:35 -0400

    Add cabin seed data

--- backend/database/seeders/CabinSeeder.php
+++ backend/database/seeders/CabinSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Cabin;
 
 class CabinSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class CabinSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        Cabin::create([
+            'name' => 'Cabin A',
+            'capacity' => 2,
+            'description' => 'Small cabin for couples.',
+            'status' => 'available',
+        ]);
+
+        Cabin::create([
+            'name' => 'Cabin B',
+            'capacity' => 4,
+            'description' => 'Family cabin with mountain view.',
+            'status' => 'available',
+        ]);
+
+        Cabin::create([
+            'name' => 'Cabin C',
+            'capacity' => 6,
+            'description' => 'Large cabin for groups.',
+            'status' => 'available',
+        ]);
+        
     }
 }
